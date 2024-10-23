c7a1b71df896d0c53704997f3b40be45f08e24d8
karmaner <2217072099@qq.com>
2024-10-23 23:39:41 +0800

    date读取错误

--- src/observer/common/value.cpp
+++ src/observer/common/value.cpp
@@ -123,6 +123,10 @@ void Value::set_data(char *data, int length)
       value_.float_value_ = *(float *)data;
       length_             = length;
     } break;
+    case AttrType::DATES: {
+      value_.date_value_ = *(date *)data;
+      length_             = length;
+    } break;
     case AttrType::BOOLEANS: {
       value_.bool_value_ = *(int *)data != 0;
       length_            = length;
